d464bca3ea8d5554315fc41b299f1352970eead3
ridhisha1996 <ridhimasharma66092@gmail.com>
2019-03-07 12:27:32 +0530

    try

--- app/src/main/java/com/ridhi/acer/github/MainActivity.java
+++ app/src/main/java/com/ridhi/acer/github/MainActivity.java
@@ -2,12 +2,24 @@ package com.ridhi.acer.github;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed = findViewById(R.id.edit);
+    }
+
+    public void click(View view) {
+        String a = ed.getText().toString();
+        Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
     }
 }
